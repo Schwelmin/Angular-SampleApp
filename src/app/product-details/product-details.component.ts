c83cf0df5fcbdd8d49527ec47d0b837d14b0f9d9
Schwelmer <davidweidtke@gmail.com>
2020-06-21 14:20:33 +0200

    cart fix

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -12,7 +13,8 @@ export class ProductDetailsComponent implements OnInit {
   product;
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private cartService: CartService
   ) { }
 
   addToCart(product) {
